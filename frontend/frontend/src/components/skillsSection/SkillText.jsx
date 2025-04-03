8b3dd435986bf03c299110346231deb67433ca7b
harshit sharma <harshsharmavatasa@gmail.com>
2025-04-03 00:59:07 -0700

    d

--- frontend/frontend/src/components/skillsSection/SkillText.jsx
+++ frontend/frontend/src/components/skillsSection/SkillText.jsx
@@ -3,7 +3,7 @@ const SkillsText = () => {
     return (
       <div className="flex flex-col items-center mt-[100px]">
         <h2 className="text-6xl font-semibold text-cyan mb-10">My Skills</h2>
-        <p className="text-lg text-center text-[rgb(97,79,160)]">
+        <p className="text-6xl font-bold  text-center text-[rgb(53,255,120)]">
         These are my Foundation Skills
         </p>
       </div>
