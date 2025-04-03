b56318b6767d7a29f378c08d48353df23d30d32e
harshit sharma <harshsharmavatasa@gmail.com>
2025-04-02 23:56:50 -0700

    hh

--- frontend/frontend/src/components/skillsSection/AllskillsSM.jsx
+++ frontend/frontend/src/components/skillsSection/AllskillsSM.jsx
@@ -87,15 +87,23 @@
 
 
 
-
 import { FaHtml5 } from "react-icons/fa";
 import { FaCss3Alt } from "react-icons/fa";
-import { IoLogoJavascript } from "react-icons/io";
+import { IoLogoJavascript } from "react-icons/io5";
 import { SiTypescript } from "react-icons/si";
 import { FaReact } from "react-icons/fa";
 import { SiRedux } from "react-icons/si";
 import { SiNextdotjs } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
 import { RiTailwindCssFill } from "react-icons/ri";
+import { SiGreensock } from "react-icons/si";
+import { SiCplusplus } from "react-icons/si";
+import { FaPython } from "react-icons/fa";
+import { FaGitAlt } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { SiPostman } from "react-icons/si";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../frameMotion/variant";
 import React from "react";
@@ -108,7 +116,16 @@ const skills = [
   { skill: "ReactJS", icon: FaReact },
   { skill: "Redux", icon: SiRedux },
   { skill: "NextJS", icon: SiNextdotjs },
+  { skill: "NodeJS", icon: FaNodeJs },
+  { skill: "ExpressJS", icon: SiExpress },
+  { skill: "MongoDB", icon: SiMongodb },
   { skill: "TailwindCSS", icon: RiTailwindCssFill },
+  { skill: "GSAP", icon: SiGreensock },
+  { skill: "C++", icon: SiCplusplus },
+  { skill: "Python", icon: FaPython },
+  { skill: "Git", icon: FaGitAlt },
+  { skill: "GitHub", icon: FaGithub },
+  { skill: "Postman", icon: SiPostman },
 ];
 
 const AllSkillsSM = () => {
