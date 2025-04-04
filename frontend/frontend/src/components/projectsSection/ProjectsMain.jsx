9752a633356a865f81279a1ea3d3ed19445ec659
harshit sharma <harshsharmavatasa@gmail.com>
2025-04-04 06:17:07 -0700

    h

--- frontend/frontend/src/components/projectsSection/ProjectsMain.jsx
+++ frontend/frontend/src/components/projectsSection/ProjectsMain.jsx
@@ -119,7 +119,7 @@ const projects = [
     year: "Feb 2025",
     align: "left",
     image: "/images/HeroVerse.png",
-    link: "https://heroverse-chi.vercel.app/",
+    link: "https://heroverse-kappa.vercel.app/",
   },
 ];
 
